671c19d6b28f47dd749d24b6a3d41503b51211d8
Sameer Ansari <sam3268212@gmail.com>
2025-05-29 16:38:19 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const cron = require('node-cron');
 const { PORT } = require('./config/serverConfig');
 
 const { sendBasicEmail } = require('./services/email-service');
@@ -13,12 +14,15 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'sam2128623@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'sam2128623@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+        cron.schedule('*/5 * * * *', () => {
+            console.log('running a task every 5 minutes');
+        });
     });
 }
 
