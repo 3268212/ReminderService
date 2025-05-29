6b0532761b206d444763d0c1bf0d29858f445976
Sameer Ansari <sam3268212@gmail.com>
2025-05-29 22:50:53 +0530

    Added notification crone

--- src/index.js
+++ src/index.js
@@ -4,25 +4,29 @@ const bodyParser = require('body-parser');
 const cron = require('node-cron');
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+//const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
+
+ 
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-
+        jobs();
         // sendBasicEmail(
         //     'support@admin.com',
         //     'sam2128623@gmail.com',
         //     'This is a testing email',
         //     'Hey, how are you, I hope you like the support'
         // );
-        cron.schedule('*/5 * * * *', () => {
-            console.log('running a task every 5 minutes');
-        });
+        
     });
 }
 
